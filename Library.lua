a94c0b8a70d5ade9efe96f79343b8b223002ff65
llocalsskoi <artemfaizov122@gmail.com>
2026-05-01 01:10:49 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -32,7 +32,7 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(12, 12, 12);
     BackgroundColor = Color3.fromRGB(8, 8, 8);
-    AccentColor = Color3.fromRGB(35, 35, 35);
+    AccentColor = Color3.fromRGB(50, 50, 50);
     OutlineColor = Color3.fromRGB(12, 12, 12);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
